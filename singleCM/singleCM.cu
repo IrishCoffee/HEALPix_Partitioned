45f0466ff3984bade0ddd6c4e8e0b2ada8a70353
IrishCoffee <IrishCoffee1006@gmail.com>
2015-09-09 00:02:32 +0800

    singleCM perfect version

--- singleCM/singleCM.cu
+++ singleCM/singleCM.cu
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <cstring>
 #include "tbb/parallel_sort.h"
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
@@ -19,7 +20,7 @@ int ref_line[20];
 char ref_file[20][16];
 int sam_line[20];
 char sam_file[20][16];
-const int GPU_N = 4;
+const int GPU_N = 2;
 const int GBSize = 1024 * 1024 * 1024;
 const int block_size = 512;
 const int TILE_SIZE = 1024;
@@ -28,6 +29,7 @@ struct NODE
 	double ra,dec;
 	int pix;
 };
+const int cntSize = 805306368;
 double diffTime(timeval start,timeval end)
 {
 	return (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) * 0.001;
@@ -55,6 +57,24 @@ int begin_index(int key, NODE *node, int N)
 	return N;
 }
 
+	__host__ __device__
+int get_start(int key,NODE *node,int N)
+{
+	for(int i = 0; i < N; ++i)
+	{
+		if(node[i].pix >= key)
+			return i;
+	}
+	return N;
+}
+__host__ __device__
+int get_end(int key,NODE *node,int N)
+{
+	for(int i = 0; i < N; ++i)
+		if(node[i].pix > key)
+			return i;
+	return N;
+}
 	__host__ __device__
 int binary_search(int key, NODE *node, int N)
 {
@@ -107,6 +127,9 @@ void kernel_singleCM(NODE *ref_node, int ref_N, NODE *sam_node, int sam_N, int *
 	__shared__ int iteration;
 
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	if(tid < sam_N)
+		sam_matchedCnt[tid] = 0;
+
 
 	if(threadIdx.x == 0)
 	{
@@ -124,6 +147,7 @@ void kernel_singleCM(NODE *ref_node, int ref_N, NODE *sam_node, int sam_N, int *
 			start_ref_pos = binary_search(start_pix - 1,ref_node,ref_N);
 
 		end_ref_pos = binary_search(end_pix,ref_node,ref_N);
+
 		if(end_ref_pos == -1)
 			end_ref_pos = ref_N - 1;
 		else
@@ -131,12 +155,10 @@ void kernel_singleCM(NODE *ref_node, int ref_N, NODE *sam_node, int sam_N, int *
 		block_ref_N = end_ref_pos - start_ref_pos + 1;
 		iteration = ceil(block_ref_N * 1.0 / TILE_SIZE);
 	}
-	if(threadIdx.x == 0 && blockIdx.x == gridDim.x - 1)
-		printf("block_sam_N %d start_ref_pos %d end_ref_pos %d iteration %d\n",block_sam_N,start_ref_pos,end_ref_pos,iteration);
-
 	__syncthreads();
-	if(start_ref_pos == -1)
+	if(start_ref_pos == -1 || end_ref_pos < start_ref_pos)
 		return;
+
 	int pix,cnt = 0;
 	double sam_ra,sam_dec;
 	if(tid < sam_N)
@@ -172,7 +194,7 @@ void kernel_singleCM(NODE *ref_node, int ref_N, NODE *sam_node, int sam_N, int *
 				break;
 			if(s_ref_pix[j] < pix)
 				continue;
-			if(matched(sam_ra,sam_dec,s_ref_ra[j],s_ref_dec[j],0.0056))
+			if(s_ref_pix[j] == pix && matched(sam_ra,sam_dec,s_ref_ra[j],s_ref_dec[j],0.0056))
 			{
 				cnt++;
 				if(cnt <= 5)
@@ -189,8 +211,7 @@ void kernel_singleCM(NODE *ref_node, int ref_N, NODE *sam_node, int sam_N, int *
 void singleCM(NODE h_ref_node[], int ref_N, NODE h_sam_node[], int sam_N, int h_sam_match[],int h_sam_matchedCnt[])
 {
 	//the maximum number of sample points that can be matched each time by each card
-//	int part_sam_N = 25000000;
-	int part_sam_N = 20000000;
+	int part_sam_N = 25000000;
 	int part_ref_N = 8 * part_sam_N;
 
 	NODE *d_ref_node[GPU_N];
@@ -212,7 +233,6 @@ void singleCM(NODE h_ref_node[], int ref_N, NODE h_sam_node[], int sam_N, int h_
 		checkCudaErrors(cudaMalloc(&d_sam_node[i],sizeof(NODE) * part_sam_N));
 		checkCudaErrors(cudaMalloc(&d_sam_match[i],sizeof(int) * part_sam_N  * 5));
 		checkCudaErrors(cudaMalloc(&d_sam_matchedCnt[i],sizeof(int) * part_sam_N));
-		checkCudaErrors(cudaMemset(d_sam_matchedCnt[i],0,sizeof(int) * part_sam_N));
 
 		checkCudaErrors(cudaMemGetInfo(&free_mem,&total_mem));
 		printf("Card %d after malloc %.2lf GB, total memory %.2lf GB\n",i,free_mem * 1.0 / GBSize,total_mem * 1.0 / GBSize);
@@ -245,9 +265,10 @@ void singleCM(NODE h_ref_node[], int ref_N, NODE h_sam_node[], int sam_N, int h_
 				start_ref_pos = 0;
 			else
 				start_ref_pos = binary_search(start_pix - 1,h_ref_node,ref_N);
+			//				start_ref_pos = get_start(start_pix,h_ref_node,ref_N);
 
 			if(start_ref_pos == -1)
-				break;
+				continue;
 			int end_ref_pos = binary_search(end_pix,h_ref_node,ref_N) - 1;
 			if(end_ref_pos == -2)
 				end_ref_pos = ref_N - 1;
@@ -256,11 +277,13 @@ void singleCM(NODE h_ref_node[], int ref_N, NODE h_sam_node[], int sam_N, int h_
 			dim3 block(block_size);
 			dim3 grid(min(65536,(int)ceil(cur_sam_N * 1.0 / block.x)));
 
+			if(cur_ref_N == 0)
+				continue;
+
 			printf("\n\nCard %d iteration %d\n",i,ite);
 			printf("block.x %d grid.x %d\n",block.x,grid.x);
 			printf("start_sam_pos %d start_sam_pix %d end_sam_pos %d end_sam_pix %d sam_N %d\n",start_sam_pos,start_pix,end_sam_pos,end_pix,cur_sam_N);
 			printf("start_ref_pos %d start_ref_pix %d end_ref_pos %d end_ref_pix %d ref_N %d\n",start_ref_pos,h_ref_node[start_ref_pos].pix,end_ref_pos,h_ref_node[end_ref_pos].pix,cur_ref_N);
-
 			checkCudaErrors(cudaMemcpy(d_sam_node[i],h_sam_node + start_sam_pos,cur_sam_N * sizeof(NODE),cudaMemcpyHostToDevice));
 			checkCudaErrors(cudaMemcpy(d_ref_node[i],h_ref_node + start_ref_pos,cur_ref_N * sizeof(NODE), cudaMemcpyHostToDevice));
 			kernel_singleCM<<<grid,block>>>(d_ref_node[i],cur_ref_N,d_sam_node[i],cur_sam_N,d_sam_match[i],d_sam_matchedCnt[i],start_ref_pos,start_sam_pos);
@@ -274,34 +297,20 @@ void singleCM(NODE h_ref_node[], int ref_N, NODE h_sam_node[], int sam_N, int h_
 	for(int i = sam_N - 1; i >= 0; --i)
 	{
 		sum += h_sam_matchedCnt[i];
-		cnt[h_sam_matchedCnt[i]]++;
-		if(i >= 100)
-			continue;
 		/*
-		cout << i << " " << h_sam_matchedCnt[i] << endl;
-		cout << h_sam_node[i].ra << " " << h_sam_node[i].dec << endl;
-		cout << "\n----------------\n" << endl;
-		for(int j = i * 5; j < i * 5 + min(5,h_sam_matchedCnt[i]); ++j)
-		{
-			int pos = h_sam_match[j];
-			cout << h_ref_node[pos].ra << " " << h_ref_node[pos].dec << endl;
-		}
-		cout << "\n--------------------\n" << endl;
-		*/
+		   cout << i << " " << h_sam_matchedCnt[i] << endl;
+		   cout << h_sam_node[i].ra << " " << h_sam_node[i].dec << endl;
+		   cout << "\n----------------\n" << endl;
+		   for(int j = i * 5; j < i * 5 + min(5,h_sam_matchedCnt[i]); ++j)
+		   {
+		   int pos = h_sam_match[j];
+		   cout << h_ref_node[pos].ra << " " << h_ref_node[pos].dec << endl;
+		   }
+		   cout << "\n--------------------\n" << endl;
+		 */
 	}
 	cout << "sum " << sum << endl;
 	cout << "ave " << sum * 1.0 / sam_N << endl;
-	unsigned long long sum1 = 0;
-	int N = 0;
-	for(int i = 0; i < 1000; ++i)
-		if(cnt[i])
-		{
-			N += cnt[i];
-			sum1 += cnt[i] * i;
-		//	printf("%d %d\n",i,cnt[i]);
-		}
-
-	cout << " N " << N << " sum1 " << sum1 << " ave " << sum1 * 1.0 / N << endl;
 }
 
 
@@ -309,9 +318,9 @@ int main(int argc, char *argv[])
 {
 	struct timeval start,end;
 
-	const int ref_N = 1538557680;
-	const int sam_N = 200006373;
-//	const int ref_N = 200006373;
+	int ref_N = atoi(argv[3]);
+	int sam_N = atoi(argv[4]);
+	//	const int ref_N = 200006373;
 	time_t rawtime;
 
 	FILE *fd = fopen(argv[1],"r");
@@ -362,17 +371,39 @@ int main(int argc, char *argv[])
 
 	gettimeofday(&start,NULL);
 	tbb::parallel_sort(ref_node,ref_node + ref_N,cmp);
+	tbb::parallel_sort(sam_node,sam_node + sam_N,cmp);
 	gettimeofday(&end,NULL);
 	printf("sort time %.3f \n",diffTime(start,end) * 0.001);
 
 	time(&rawtime);
 	printf("after sort : %s\n",ctime(&rawtime));
-
+	
 	gettimeofday(&start,NULL);
 	singleCM(ref_node,ref_N,sam_node,sam_N,sam_match,sam_matchedCnt);
 	gettimeofday(&end,NULL);
+
 	printf("single CM %.3f s\n",diffTime(start,end) * 0.001);
 	printf("single CM %.3f min\n",diffTime(start,end) * 0.001 / 60);
 	time(&rawtime);
 	printf("singleCM : %s\n",ctime(&rawtime));
+
+	
+	int *ref_match = (int*)malloc(sizeof(int) * ref_N * 5);
+	int *ref_matchedCnt = (int*)malloc(sizeof(int) * ref_N);
+
+	gettimeofday(&start,NULL);
+	singleCM(sam_node,sam_N,ref_node,ref_N,ref_match,ref_matchedCnt);
+	gettimeofday(&end,NULL);
+	printf("singe CM-2 %.3f s\n",diffTime(start,end) * 0.001);
+
+
+
+	free(sam_node);
+	free(ref_node);
+	free(ref_match);
+	free(sam_match);
+	free(ref_matchedCnt);
+	free(sam_matchedCnt);
+	
+	return 0;
 }
